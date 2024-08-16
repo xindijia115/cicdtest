6bf4381f857ef93a86788346d4818c27bba7dcb5
DJ <xindijia@qt.com>
2024-08-16 21:34:03 +0800

    feat[cicd]:cicd测试06

--- src/main/java/com/example/cicdtest/controller/HelloController.java
+++ src/main/java/com/example/cicdtest/controller/HelloController.java
@@ -17,6 +17,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello dj hahaha zzbb aaaa";
+        return "hello dj hahaha zzbb aaaa why";
     }
 }
